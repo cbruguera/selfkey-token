55bf7597989ff19628f37800bef697cf4c0d34a8
Carlos Bruguera <cbruguera@gmail.com>
2018-02-13 12:13:01 +0700

    updated parameters for airdrop deployment

--- migrations/3_deploy_airdrop.js
+++ migrations/3_deploy_airdrop.js
@@ -1,8 +1,8 @@
 const SelfKeyAirdrop = artifacts.require('./SelfKeyAirdrop.sol')
 
 module.exports = (deployer) => {
-  const crowdsaleAddress = ''
-  const tokenAddress = ''
+  const crowdsaleAddress = '0x8d4b2cffe2dcbbf06eaa5920401db86ca5fb8177'
+  const tokenAddress = '0x4cc19356f2d37338b9802aa8e8fc58b0373296e7'
 
   deployer.deploy(
     SelfKeyAirdrop,
